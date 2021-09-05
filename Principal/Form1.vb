c266e8b712756b0605ae79c029d63bc660c26407
Orazio Narducci <orazio.narducci@gmail.com>
2021-09-05 19:31:47 -0300

    Comentário

--- Principal/Form1.vb
+++ Principal/Form1.vb
@@ -1,6 +1,7 @@
 ﻿Imports Cadastros
 Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Abre o form cliente para teste
         Dim frm As New frmCadCliente
         frm.Show()
     End Sub
